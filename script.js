8e8d2f006a1c5a7672356bb09847e65960b64d32
warden44 <warden44nick@gmail.com>
2022-10-06 19:52:12 -0600

    add button and random color

--- script.js
+++ script.js
@@ -1,23 +1,26 @@
 // import randColor from "./randomColor";
 const canvas = document.getElementById("canvas1");
+
 const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-const randomColor = Math.floor(Math.random() * 255);
+const randomGreen = Math.floor(Math.random() * 255);
+const randomRed = Math.floor(Math.random() * 255);
+const randomBlue = Math.floor(Math.random() * 255);
+
 const maxLevel = 4;
 const branches = 3;
 const sides = Math.floor(Math.random() * 10 + 3);
 const spread = Math.random() * 48 + 0.51;
 
 ctx.translate(canvas.width / 2, canvas.height / 2);
-
 const angle = Math.PI * 2 * spread;
 
 function drawLine(level) {
   if (level > maxLevel) return;
 
-  //   ctx.strokeStyle = `rgb(${(randomColor, randomColor, randomColor)})`;
-  ctx.strokeStyle = "#FF9944";
+  ctx.strokeStyle = `rgb(${randomRed},${randomGreen},${randomBlue})`;
+  //   ctx.strokeStyle = "#FF9944";
   ctx.lineWidth = 2;
   ctx.beginPath();
   ctx.moveTo(0, 0);
